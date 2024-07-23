b5eb960fed8522c8fd8c6f74312237e079eb18cc
Farzaneh <farzanekazemi8517@gmail.com>
2024-07-23 19:34:38 +0330

    comment json-server

--- server.cjs
+++ server.cjs
@@ -1,5 +1,5 @@
 // JSON Server module
-const jsonServer = require("json-server");
+// const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
