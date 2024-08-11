f0b6236763d9857539154033fcd418c79dae069b
Farzaneh <farzanekazemi8517@gmail.com>
2024-08-11 21:20:14 +0330

    install json-server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // JSON Server module
-// const jsonServer = require("json-server");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
@@ -9,15 +9,15 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
 );
 server.use(router);
 // Listen to port
 server.listen(8000, () => {
- console.log("JSON Server is running");
+  console.log("JSON Server is running");
 });
 
 // Export the Server API
